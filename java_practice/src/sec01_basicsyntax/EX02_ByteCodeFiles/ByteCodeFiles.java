299a027ee50ffb065556daab1b546d73c7cbabd6
jieungrace <jeyeom10@naver.com>
2022-07-13 00:26:06 +0900

    220713

---
+++ java_practice/src/sec01_basicsyntax/EX02_ByteCodeFiles/ByteCodeFiles.java
@@ -0,0 +1,28 @@
+// 컴파일 후 생성되는 바이트코드 (.class)
+
+package sec01_basicsyntax.EX02_ByteCodeFiles;
+
+class A { // A.class
+} 
+class B { // B.class
+} 
+class C { // C.class
+	class D { // C$D.class - C는 아우터클래스, D는 이너클래
+	}
+	} 
+
+public class ByteCodeFiles {
+	
+	public static void main(String[] args) {
+	
+		System.out.println("컴파일후 생성되는 바이트코드 확인");
+		
+		
+	} //1개의 소스파일에는 최대 1개의 public class, 여러개의 class 구성 가능
+	
+	
+	
+	
+
+
+}
