de20944aaf3831555dfbe4ae53f4b9796f91ab7d
jieungrace <jeyeom10@naver.com>
2022-07-13 00:14:20 +0900

    220713 에러포

--- java_practice/src/sec01_basicsyntax/EX01_JavaBasicStructure/JavaBasicStructure.java
+++ java_practice/src/sec01_basicsyntax/EX01_JavaBasicStructure/JavaBasicStructure.java
@@ -1,5 +1,20 @@
 package sec01_basicsyntax.EX01_JavaBasicStructure;
 
 public class JavaBasicStructure {
+	
+	public class JavaBasicStrcture {
+		public static void main(String[] args) {
+//			1줄 주석
+			System.out.println("콘솔 화면에 출력");
+			
+		}
+	}
 
 }
+/* 최초 run 진행시 
+the selection cannot be launched, 
+and there are no recent launches.abstrac
+오류가 발생하였음
+검색결 default 정의 문제로 확인
+preferences > rundebug > launching > launch operation 항목에서
+launch the associated project 로 바꿔준 후 해결 */
