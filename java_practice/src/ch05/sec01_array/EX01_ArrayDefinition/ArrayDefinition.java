08ff04755fb751b06a81486e24458f765fc7e06c
jieungrace <jeyeom10@naver.com>
2022-07-18 14:52:50 +0900

    배

---
+++ java_practice/src/ch05/sec01_array/EX01_ArrayDefinition/ArrayDefinition.java
@@ -0,0 +1,22 @@
+package ch05.sec01_array.EX01_ArrayDefinition;
+
+public class ArrayDefinition {
+	
+	public static void main(String[] args_) {
+		
+		int[] array1 = new int [3]; // 정수형 array1 배열을 만들고 그안에 정수 3개 저장
+		int[] array2;
+		array2=new int[3];
+		
+		int array3[] = new int [3];
+		int array4[];
+		array4 = new int[3];
+		
+		
+		boolean[] array5=new boolean[3];
+		int[] array6=new int[5];
+		double[] array7 = new double[7];
+		String[] array8 = new String[9]; 
+	}
+
+}
