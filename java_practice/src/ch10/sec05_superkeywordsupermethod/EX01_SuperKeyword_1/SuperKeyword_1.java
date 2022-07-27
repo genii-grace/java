f40c0ff337194af3a28135d6a307f331192e2228
jieungrace <jeyeom10@naver.com>
2022-07-27 23:52:19 +0900

    super keyword

---
+++ java_practice/src/ch10/sec05_superkeywordsupermethod/EX01_SuperKeyword_1/SuperKeyword_1.java
@@ -0,0 +1,15 @@
+package ch10.sec05_superkeywordsupermethod.EX01_SuperKeyword_1;
+
+class A{
+	void abc() {
+		System.out.println("abc() of class A");
+	}
+}
+public class SuperKeyword_1 {
+
+	public static void main(String[] args) {
+
+
+	}
+
+}
