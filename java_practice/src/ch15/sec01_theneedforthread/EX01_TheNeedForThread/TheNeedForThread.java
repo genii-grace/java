5db7ab8a5ddce9ea13c01b6b02e19347d6148f44
jieungrace <jeyeom10@naver.com>
2022-08-02 20:32:39 +0900

    단일쓰레드

---
+++ java_practice/src/ch15/sec01_theneedforthread/EX01_TheNeedForThread/TheNeedForThread.java
@@ -0,0 +1,23 @@
+package ch15.sec01_theneedforthread.EX01_TheNeedForThread;
+
+public class TheNeedForThread {
+
+	public static void main(String[] args) {
+	
+		int[] intArray= {1,2,3,4,5};
+		
+		String[] strArray= {"하나","둘","셋","넷","다섯"};
+		
+		for(int i=0; i<intArray.length; i++) {
+			System.out.println("(비디오프레임) "+intArray[i]);
+			try {Thread.sleep(200);} catch(InterruptedException e) {}
+		}
+		
+		for(int i=0; i<strArray.length; i++) {
+			System.out.println("(자막번호) "+strArray[i]);
+			try {Thread.sleep(200);} catch (InterruptedException e) {}
+		}
+
+	}
+
+}
