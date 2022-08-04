9e1219b9ba47596d36eeae6dc1c44cd7ad1f7b66
jieungrace <jeyeom10@naver.com>
2022-08-05 00:29:47 +0900

    제네릭 메서드에서 제네릭 타입 제한범위 설정

---
+++ java_practice/src/ch16/sec04_boundedtype/EX02_BoundedTypeOfGenericMethod/BoundedTypeOfGenericMethod.java
@@ -0,0 +1,40 @@
+package ch16.sec04_boundedtype.EX02_BoundedTypeOfGenericMethod;
+
+class A{
+	public <T extends Number> void method1(T t) {
+		System.out.println(t.intValue());
+	}
+}
+
+interface MyInterface{
+	public abstract void print();
+}
+
+class B{
+	public <T extends MyInterface> void method1(T t) {
+		t.print();
+	}
+}
+
+public class BoundedTypeOfGenericMethod {
+
+	public static void main(String[] args) {
+
+
+		A a=new A();
+		a.method1(5.8);
+		
+		B b=new B();
+		b.method1(new MyInterface(){ //인터페이스 익명이너클래스로 객체 
+		@Override
+		public void print() {
+			System.out.println("print() 구현");
+			}
+		
+		});
+		
+		
+
+	}	
+	
+}
