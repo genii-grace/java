581e8f7014b4850e51a8bc3969cd46e4b4a66707
jieungrace <jeyeom10@naver.com>
2022-08-13 16:37:46 +0900

    file inputstream

---
+++ java_practice/src/ch19/sec02_fileinputoutputStream/EX03_FileInputStream_3/FileInputStream_3.java
@@ -0,0 +1,43 @@
+package ch19.sec02_fileinputoutputStream.EX03_FileInputStream_3;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+
+public class FileInputStream_3 {
+
+	public static void main(String[] args) throws IOException {
+
+		File inFile=new File("chap19/FileInputStream2.txt");
+		
+		InputStream is2=new FileInputStream(inFile);
+		byte[] byteArray1=new byte[9];
+		int count1;
+		while( (count1=is2.read(byteArray1))!= -1) {
+			String str=new String(byteArray1, 0, count1, Charset.forName("UTF-8"));
+			System.out.print(str);
+			System.out.println(" : count = " + count1);
+		}
+		
+		
+		is2.close();
+		
+		System.out.println();
+
+		System.out.println();
+		
+		InputStream is3=new FileInputStream(inFile);
+		byte[] byteArray2=new byte[9];
+		int offset=3;
+		int length=6;
+		int count2=is3.read(byteArray2,offset,length);
+		String str=new String(byteArray2, 0,offset+count2, Charset.defaultCharset());
+		
+		System.out.println(str);
+		is3.close();
+		
+	}
+
+}
