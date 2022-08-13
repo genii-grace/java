dce6f75d68749c388665e47d048ff17cee053d14
jieungrace <jeyeom10@naver.com>
2022-08-13 19:50:58 +0900

    콘솔 영문 입출력

--- java_practice/src/ch19/sec03_consoleinputoutput/EX01_ConsoleInputObject_1/ConsoleInputObject_1.java
+++ java_practice/src/ch19/sec03_consoleinputoutput/EX01_ConsoleInputObject_1/ConsoleInputObject_1.java
@@ -10,7 +10,7 @@ public class ConsoleInputObject_1 {
 		InputStream is =System.in;
 		int data;
 		while((data=is.read()) != '\n') {
-			System.out.println("읽은 데이터 : "+(char)data + "남은 바이트 수 : "+is.available());
+		System.out.println("읽은 데이터 : "+(char)data + "남은 바이트 수 : "+is.available());
 		}
 		
 		System.out.println(data);
