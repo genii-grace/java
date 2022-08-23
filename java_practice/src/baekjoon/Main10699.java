d4f38c60a8fc0764739c8431f0912d1a31f5ed53
jieungrace <jeyeom10@naver.com>
2022-08-23 13:18:40 +0900

    boj 10699

--- java_practice/src/baekjoon/Main10699.java
+++ java_practice/src/baekjoon/Main10699.java
@@ -1,28 +1,15 @@
 package baekjoon;
 
-/*
-import java.time.LocalDate;
-
-public class Main10699 {
-
-	public static void main(String[] args) {
-		
-		LocalDate now = LocalDate.now();
-		System.out.println(now);
-		
-	}
-	*/
 import java.text.SimpleDateFormat;
 import java.util.*;
 
 public class Main10699{
 	public static void main(String[] args) {
 		
-		Scanner sc=new Scanner(System.in);
-		SimpleDateFormat format= new SimpleDateFormat("yyyy-MM-dd");
+		SimpleDateFormat newFormat= new SimpleDateFormat("yyyy-MM-dd");
 		
 		Date d=new Date();
-		System.out.println(format.format(d));
+		System.out.println(newFormat.format(d));
 		
 	}
 
