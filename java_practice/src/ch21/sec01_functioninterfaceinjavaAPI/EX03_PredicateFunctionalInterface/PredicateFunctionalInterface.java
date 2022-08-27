1f4d0ae104bb1f21e827caf48905fa5434a99122
jieungrace <jeyeom10@naver.com>
2022-08-27 21:29:56 +0900

    PredicateFunctionInterface

---
+++ java_practice/src/ch21/sec01_functioninterfaceinjavaAPI/EX03_PredicateFunctionalInterface/PredicateFunctionalInterface.java
@@ -0,0 +1,10 @@
+package ch21.sec01_functioninterfaceinjavaAPI.EX03_PredicateFunctionalInterface;
+
+public class PredicateFunctionalInterface {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
