86358fad5c72dba0300854dfe6ddc0641f0bea3f
genii grace <jeyeom10@naver.com>
2022-09-06 16:23:12 +0900

    boj

---
+++ src/baekjoon/Main.java
@@ -0,0 +1,35 @@
+package baekjoon;
+
+public class Main {
+	
+    public static void main(String[] args){
+//    	2557
+    	/*
+        System.out.print("Hello World!");
+    	 */
+    	
+//    	10718 
+    	/*
+        System.out.println("강한친구 대한육군");
+        System.out.print("강한친구 대한육군");
+         */
+
+//    	10171
+    	/*
+    	System.out.println("\\    /\\\n )  ( \')\n(  /  )\n \\(__)|");
+    	*/
+    	
+//    	10172
+    	/*
+    	System.out.println("|\\_/|\n|q p|   /}\n( 0 )\"\"\"\\\n|\"^\"`    |\n||_/=\\\\__|\n");
+	*/
+    	
+//    	1000
+    	
+
+    	
+    	
+    	
+    }
+
+}
