599e514b56c2cf2e39fb4f21e41c9f8e25576e9a
genii grace <jeyeom10@naver.com>
2022-09-21 22:44:46 +0900

    mega allcare

---
+++ AllCareJava/src/Day01/HelloWorld.java
@@ -0,0 +1,27 @@
+package Day01;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		//print, println, printf 차이
+		System.out.print("Hello World");
+		System.out.print("안녕하세요");
+		System.out.println("안녕히 계세요");
+		System.out.printf("오늘 날씨는 %d도 입니다.",24);
+		
+		//변수와 자료형 타입
+		int a = 10;
+		System.out.println(a);
+		
+		a= 20;
+		System.out.println(a);
+		
+		a='A';
+		System.out.println(a);
+		
+		a='가';
+		System.out.println(a);
+	}
+
+}
