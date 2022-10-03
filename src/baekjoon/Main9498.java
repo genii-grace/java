c0f943d4876c0de6342e56535ef4e3de2e29e220
genii grace <jeyeom10@naver.com>
2022-10-03 23:36:37 +0900

    boj

---
+++ src/baekjoon/Main9498.java
@@ -0,0 +1,15 @@
+package baekjoon;
+
+public class Main9498 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/*
+		 * 시험점수 입력받아 
+		 * 90~100 A, 80~89 B, 70~79 C, 670!69 D, 나머지 F 출력 
+		 */
+
+	}
+
+}
