e430f54d46b91b3c333d8f58ede54a6d38732b3e
genii grace <jeyeom10@naver.com>
2022-10-06 23:47:52 +0900

    Day11 method 예제

---
+++ AllCareJava/src/Day11/MethodTestArray.java
@@ -0,0 +1,22 @@
+package Day11;
+
+public class MethodTestArray {
+
+	
+	static int[][] test(){
+		int[][][][] a = new int[2][2][2][2];
+		return a[0][0];
+	}
+	
+	static int test1() {
+		int[][][][] a = new int[2][2][2][2];
+		return a[0][0][0][0];
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
