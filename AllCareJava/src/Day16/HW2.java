5283a141d19144a4555503dfd453041af0d67470
genii grace <jeyeom10@naver.com>
2022-10-17 14:00:56 +0900

    미완성 Day16 HW 문방구 문제

---
+++ AllCareJava/src/Day16/HW2.java
@@ -0,0 +1,66 @@
+package Day16;
+
+class Shop{
+
+	//문방구에 색연필, 지우개, 펜 나열하기 
+	ColorPen[] CPlist = new ColorPen[4];
+	String[] CP = {"주황색","초록색","노란색","파란색"};
+	
+	Eraser[] Elist = new Eraser[2];
+	String[] E = {"회색","빨간색"};
+	
+	Pen[] Plist = new Pen[4];
+	String[] P = {"주황색","초록색","노란색","파란색"};
+	
+	void make() {
+		
+		for(int i=0; i<CPlist.length; i++) {
+			CPlist[i]= new 
+		}
+		
+	}
+
+
+}
+
+class ColorPen{
+
+	String color;
+
+	ColorPen(String color){
+		this.color=color;
+	}
+
+}
+
+class Eraser{
+
+	String color;
+	Eraser(String color){
+		this.color=color;
+	}
+
+}
+
+class Pen{
+
+	String color;
+	Pen(String color){
+		this.color=color;
+	}
+
+}
+
+class Student{
+
+}
+
+
+public class HW2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
