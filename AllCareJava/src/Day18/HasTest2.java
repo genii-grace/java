2e179d0b230667a458b6866abaf998ff040837a5
genii grace <jeyeom10@naver.com>
2022-10-19 03:50:59 +0900

    Day18 has 관계

---
+++ AllCareJava/src/Day18/HasTest2.java
@@ -0,0 +1,33 @@
+package Day18;
+
+class Tier{
+	
+}
+
+class Engine{
+	
+}
+
+class Car{
+	
+	Tier[] t;
+	Engine e;
+	
+}
+
+class Factory{
+	
+	Car c;
+	
+	
+	
+}
+
+public class HasTest2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
