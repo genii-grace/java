60ab78178f0ea95a01c083b326e3edd6af16c7f8
genii grace <jeyeom10@naver.com>
2022-10-19 03:51:24 +0900

    Day18 오버로딩 예제

---
+++ AllCareJava/src/Day18/OverloadingTest2.java
@@ -0,0 +1,26 @@
+package Day18;
+
+//생성자 오버로딩
+
+class A{
+	
+	A(int a){
+		
+	}
+	
+	A(){
+		
+	}
+}
+public class OverloadingTest2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		A a = new A(5);
+		A a1 = new A();
+		
+	}
+
+}
