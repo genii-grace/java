8e74320a28f3f67f1179b26f4cac809acce048b5
genii grace <jeyeom10@naver.com>
2022-11-03 23:26:23 +0900

    개인 실습 - interface 내 static method 실습

---
+++ AllCareJava/src/Day28/Test.java
@@ -0,0 +1,15 @@
+package Day28;
+
+interface A{
+	static void abc() {
+		System.out.println("print of interface A ");
+	}
+}
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		A.abc();
+	}
+}
