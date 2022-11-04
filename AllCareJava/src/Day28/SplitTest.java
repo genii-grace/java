6348d4ab2b720b5c10d98a43c01742020c6bc4eb
genii grace <jeyeom10@naver.com>
2022-11-04 23:10:31 +0900

    Day28 File IO 관련 스플릿 예제

---
+++ AllCareJava/src/Day28/SplitTest.java
@@ -0,0 +1,13 @@
+package Day28;
+
+public class SplitTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String a = "abc,def,xor";
+		String[] temp = a.split(",");
+
+	}
+
+}
