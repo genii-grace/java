bbbdc3d21e7131969916592fe242b2b0799efbf0
genii grace <jeyeom10@naver.com>
2022-11-18 14:10:43 +0900

    프로그래머스 카카오 코테 예시 : 못풀었음!

---
+++ AllCareJava/src/Day29/Programmers.java
@@ -0,0 +1,14 @@
+package Day29;
+
+
+
+class Solution {
+    public int[] solution(String[] id_list, String[] report, int k) {
+        int[] answer = {};
+  
+        
+        return answer;
+    }
+}
+
+	
